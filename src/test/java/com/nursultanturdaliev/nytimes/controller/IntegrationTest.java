f22d6dd521790e39eeed615b80c9d5fda63820e2
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-08-19 20:44:40 +0200

    add test - test list endpoint without authorization credentials using static assertThat function

--- src/test/java/com/nursultanturdaliev/nytimes/controller/IntegrationTest.java
+++ src/test/java/com/nursultanturdaliev/nytimes/controller/IntegrationTest.java
@@ -1,6 +1,7 @@
 package com.nursultanturdaliev.nytimes.controller;
 
 import com.nursultanturdaliev.nytimes.responses.BestSellerListResponse;
+import com.nursultanturdaliev.nytimes.responses.UnauthorizedResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,5 +30,17 @@ public class IntegrationTest {
         Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         Assertions.assertThat(response.getBody().copyright).isEqualTo("Copyright (c) 2019 The New York Times Company.  All Rights Reserved.");
         Assertions.assertThat(response.getBody().results).isNotEmpty();
+    }
+
+    @Test
+    public void testListsWithoutAuthorization() {
+
+        ResponseEntity<UnauthorizedResponse> responseEntity = testRestTemplate.getForEntity("/svc/books/v3/lists.json", UnauthorizedResponse.class);
+
+        assertThat(responseEntity.getBody()).isInstanceOf(UnauthorizedResponse.class);
+        assertThat(responseEntity.getBody().getError()).isEqualTo("Unauthorized");
+        assertThat(responseEntity.getBody().getMessage()).isEqualTo("Unauthorized");
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+        assertThat(responseEntity.getBody().getPath()).isEqualTo("/svc/books/v3/lists.json");
     }
 }
